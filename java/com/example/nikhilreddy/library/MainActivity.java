fd1ca8a58caacc05eb24ec2c1c65e706899e10da
Nikhil Reddy <33191229+nikhilreddy007@users.noreply.github.com>
2018-12-20 12:33:38 +0530

    splash screen and signup page

--- java/com/example/nikhilreddy/library/MainActivity.java
+++ java/com/example/nikhilreddy/library/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.nikhilreddy.library;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button signupButton  = (Button) findViewById(R.id._signupbutton);
+
+        final Intent signup_intent = new Intent(this, signupActivity.class);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(signup_intent);
+            }
+        });
     }
 }
